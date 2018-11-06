eba671bae23ba49bfe21338ad4068b6bf3ba45bc
gubi <gubi.ale@iod.io>
2018-11-06 13:13:19 +0100

    Some improvements

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+ini_set("memory_limit", "1G");
 header("Content-type: text/plain");
 
 require 'vendor/autoload.php';
@@ -9,7 +10,7 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 
-define("FILENAME", "resultAgrovoc_filled_20181029.xlsx");
+define("FILENAME", "resultAgrovoc_filled_20181102.xlsx");
 
 $spreadsheet = new Spreadsheet();
 $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
@@ -17,6 +18,26 @@ $spreadsheet = $reader->load(FILENAME);
 $worksheet = $spreadsheet->getActiveSheet();
 $json = [];
 $json[FILENAME] = [];
+
+/**
+ * Move an array item to the top of order
+ * @param  array                            $array                              The array to sort
+ * @param  string                           $key                                The item to move to the top
+ * @return array                                                                The sorted array
+ */
+function move_to_top(&$array, $key) {
+    return array_splice($array, array_search($key, array_keys($array)), 1) + $array;
+}
+
+/**
+ * Move an array item to the bottom of order
+ * @param  array                            $array                              The array to sort
+ * @param  string                           $key                                The item to move to the bottom
+ * @return array                                                                The sorted array
+ */
+function move_to_bottom(&$array, $key) {
+    return $array + array_splice($array, array_search($key, array_keys($array)), 1);
+}
 
 /**
  * Open an URL using cURL
@@ -28,18 +49,50 @@ function url_open($url) {
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLINFO_HEADER_OUT, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
+    curl_setopt($ch,CURLOPT_VERBOSE,true);
+
+    $output = json_decode(curl_exec($ch));
+    $output->headers = curl_getinfo($ch);
 
     if(curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 200) {
         $logger->pushHandler(new StreamHandler(getcwd() . "/curl.log", Logger::ERROR));
-        $logger->error($output);
+        $logger->error(json_encode($output));
 
-        return json_decode($output);
+        return json_encode($output);
     }
-    return json_decode($output)->data;
+    $output_array = json_decode(json_encode($output), 1);
+    $output = move_to_bottom($output_array, "data");
+    return json_decode(json_encode($output));
 
     curl_close($ch);
+}
+
+function recognise_keywords(&$arr, $index, $title, $value, $section, $column_name = null) {
+    if($section == "_labels") { // -------------------------------------------> "_labels" section
+        if(strpos($title, "__") !== false) {
+            $keywords = explode("__", $title);
+            if(trim($keywords[1]) !== "" || $column_name == "A1") {
+                $arr[$section]["row " . $index]["_keywords"][$column_name] = $keywords[1];
+            }
+        } else {
+            if(trim($value) !== "" || $column_name == "A1") {
+                $arr[$section]["row " . $index][$column_name] = ucfirst($value);
+            }
+        }
+    } elseif($section == "contents") { // ------------------------------------> "contents" section
+        if(strpos($title, "__") !== false) {
+            $keywords = explode("__", $title);
+            if(trim($value) !== "") {
+                $arr[$section]["row " . $index]["_keywords"][$keywords[1]] = $value;
+            }
+        } else {
+            if(trim($value) !== "") {
+                $arr[$section]["row " . $index][$title] = $value;
+            }
+        }
+    }
 }
 
 /**
@@ -53,51 +106,54 @@ function url_open($url) {
 function parse_xml($json, $row, $worksheet, $visible) {
     $visible_label = ($visible) ? "visible" : "not visible";
     $highestRow = $worksheet->getHighestRow(); // e.g. 10
-    $highestColumn = $worksheet->getHighestColumn(); // e.g 'F'
+    $highestColumn = $worksheet->getHighestDataColumn(); // e.g 'F'
     $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn); // e.g. 5
+
+
+    $json["status"]["code"] = http_response_code();
+    $json["status"]["date"] = date("Y-m-d H:i:s", $_SERVER["REQUEST_TIME"]);
+    // Stats
+    $json["stats"]["columns"]["count"] = $highestColumnIndex;
+    $json["stats"]["columns"]["highest"] = $highestColumn;
+    $json["stats"]["rows"]["count"] = (int)$highestRow;
 
     for($col = 1; $col <= $highestColumnIndex; $col++) {
         // The first row is used for labels
         $title = $worksheet->getCellByColumnAndRow($col, 1)->getValue();
         $value = $worksheet->getCellByColumnAndRow($col, $row->getRowIndex())->getValue();
 
-        if($row->getRowIndex() == 1) { // ---------------------------------> "_labels" section
+        if($row->getRowIndex() == 1) { // ------------------------------------> "_labels" section
             $column_name = $worksheet->getCellByColumnAndRow($col, 1)->getCoordinate();
             // Split keywords in sub-labels
-            if(strpos($title, "__") !== false) {
-                $keywords = explode("__", $title);
-                $json[$visible_label]["_labels"]["row " . $row->getRowIndex()]["_keywords"][$column_name] = $keywords[1];
-            } else {
-                $json[$visible_label]["_labels"]["row " . $row->getRowIndex()][$column_name] = $value;
-            }
-        } elseif($row->getRowIndex() > 1) { // ----------------------------> "contents" section
+            recognise_keywords($json["rows"][$visible_label], $row->getRowIndex(), $title, $value, "_labels", $column_name);
+        } elseif($row->getRowIndex() > 1) { // -------------------------------> "contents" section
             // Split keywords in sub-labels
-            if(strpos($title, "__") !== false) {
-                $keywords = explode("__", $title);
-                $json[$visible_label]["contents"]["row " . $row->getRowIndex()]["_keywords"][$keywords[1]] = $value;
-            } else {
-                $json[$visible_label]["contents"]["row " . $row->getRowIndex()][$title] = $value;
-            }
+            recognise_keywords($json["rows"][$visible_label], $row->getRowIndex(), $title, $value, "contents");
 
             if($title == "id") {
+                // $json["rows"][$visible_label]["contents"]["row " . $row->getRowIndex()]["visible"] = $visible;
+
                 $doi = substr(parse_url($value)["path"], 1);
                 $dataset_api_url = "https://dataverse.harvard.edu/api/datasets/:persistentId?persistentId=doi:" . $doi;
-                $json[$visible_label]["contents"]["row " . $row->getRowIndex()]["dataset"] = parse_url($value);
-                $json[$visible_label]["contents"]["row " . $row->getRowIndex()]["visible"] = $visible;
-                if($visible) {
-                    $json[$visible_label]["contents"]["row " . $row->getRowIndex()]["dataset"]["doi"] = $doi;
-                    $json[$visible_label]["contents"]["row " . $row->getRowIndex()]["dataset"]["dataset_api_url"] = $dataset_api_url;
 
-                    // Download datasets only for row 24, 57 and 58
-                    if($row->getRowIndex() == 24 || $row->getRowIndex() == 57 || $row->getRowIndex() == 58) {
-                        $json[$visible_label]["contents"]["row " . $row->getRowIndex()]["dataset"]["data"] = url_open($dataset_api_url);
-                    } else {
-                        $json[$visible_label]["contents"]["row " . $row->getRowIndex()]["dataset"]["data"] = null;
-                    }
+                $json["rows"][$visible_label]["contents"]["row " . $row->getRowIndex()]["dataset"]["source"]["doi"]["uri"] = parse_url($value);
+                $json["rows"][$visible_label]["contents"]["row " . $row->getRowIndex()]["dataset"]["source"]["doi"]["uri"]["value"] = $value;
+                $json["rows"][$visible_label]["contents"]["row " . $row->getRowIndex()]["dataset"]["source"]["doi"]["value"] = $doi;
+                $json["rows"][$visible_label]["contents"]["row " . $row->getRowIndex()]["dataset"]["target"]["dataset_api_url"] = $dataset_api_url;
+
+                if($visible) {
+                    // Download datasets only for first 3 rows
+                    // if(($row->getRowIndex() - 1) <= 3) {
+                        $json["rows"][$visible_label]["contents"]["row " . $row->getRowIndex()]["dataset"]["results"] = url_open($dataset_api_url);
+                    // } else {
+                    //     $json["rows"][$visible_label]["contents"]["row " . $row->getRowIndex()]["dataset"]["results"] = null;
+                    // }
                 }
             }
+            $json["rows"][$visible_label]["contents"]["row " . $row->getRowIndex()] = move_to_bottom($json["rows"][$visible_label]["contents"]["row " . $row->getRowIndex()], "dataset");
         }
     }
+    $json["rows"][$visible_label] = move_to_top($json["rows"][$visible_label], "_labels");
 
     return $json;
 }
@@ -122,7 +178,7 @@ file_put_contents(getcwd() . "/output.txt", print_r($json, true));
 
 header("Content-type: application/json");
 // Display the output as json
-print_r(json_encode($json, JSON_PRETTY_PRINT));
+// print_r(json_encode($json, JSON_PRETTY_PRINT));
 // Save the output as json
 file_put_contents(getcwd() . "/output.json", json_encode($json, JSON_PRETTY_PRINT));
 ?>
